a8ec598fbd852d49d223f735dc469ab6ac147beb
Leandro01832 <leandroleanleo@gmail.com>
2026-04-18 14:31:51 -0300

    Filtro Comum

---
+++ business/business/FiltroComum.cs
@@ -0,0 +1,16 @@
+using business;
+
+public class FiltroComum : Filtro
+{
+    
+    // O filtro comum vão tratar informaçoes comuns a todos os capitulos, como por exemplo:
+    // comentarios, curtidas, anotacoes, compartilhamentos etc.
+    // Não vai ser necessário criar um capitulo e versiculos para o filtro comum,
+    //  pois ele vai tratar informações que são comuns a todos os capítulos.
+    // Para acessar o filtro comum basta informar o numero da pasta.
+    // Filtro comum vai possuir a mesma estrutura de outros filtros,
+    //  ou seja, vai estar presente em uma camada.
+   
+    
+
+}
--- business/obj/Debug/net6.0/business.AssemblyInfo.cs
+++ business/obj/Debug/net6.0/business.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92192263371e681d1b19cf7e3478264a500970fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd556bf57a417779462a08f1c93fc6a247ced7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
